d841b04155616c37ffc1b6ccca5b569ee6708458
Wu <1257480422@qq.com>
2023-02-17 00:21:59 +0800

    文件测试

---
+++ src/CS/NEW.java
@@ -0,0 +1,18 @@
+package CS;
+
+import java.util.Scanner;
+
+public class NEW {
+   public static  void main(String[] args){
+
+      Scanner in=new Scanner(System.in);
+      System.out.println("who is your name");
+      String name=in.nextLine();
+      System.out.println("how are u");
+      int age =in.nextInt();
+      System.out.println("you name:"+name+"  you age:"+age);
+
+
+   }
+
+}
