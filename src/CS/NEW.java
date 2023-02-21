30a67c2daea40db5c9285afbe5ffac82fe875025
Wu <1257480422@qq.com>
2023-02-21 19:06:44 +0800

    基本语法课堂练习1

--- src/CS/NEW.java
+++ src/CS/NEW.java
@@ -3,16 +3,24 @@ package CS;
 import java.util.Scanner;
 
 public class NEW {
+
    public static  void main(String[] args){
 
-      Scanner in=new Scanner(System.in);
-      System.out.println("who is your name");
-      String name=in.nextLine();
-      System.out.println("how are u");
-      int age =in.nextInt();
-      System.out.println("you name:"+name+"  you age:"+age);
-
-
+      //Scanner in=new Scanner(System.in);
+     // System.out.println("who is your name");
+      //String name=in.nextLine();
+      //System.out.println("how are u");
+      //int age =in.nextInt();
+      //System.out.println("you name:"+name+"  you age:"+age);
+      int sum=0;
+      int i=1;
+      while(i<=100){
+         if(i%2==0){
+            sum+=i;
+         }
+         i++;
+      }
+   System.out.println("100以内的偶数是："+sum);
    }
 
 }
